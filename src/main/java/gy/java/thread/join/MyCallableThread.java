7fec3b7b8a3c1486b8f77dbec5402f9e0af2db44
yantao <tao.yan@picahealth.com>
2018-05-16 15:50:37 +0800

    线程的第三种创建方式。

--- src/main/java/gy/java/thread/join/MyCallableThread.java
+++ src/main/java/gy/java/thread/join/MyCallableThread.java
@@ -12,21 +12,19 @@ public class MyCallableThread {
 				return "ok";
 			}
 		});
-		{
-			/*
-			Returns true if this task completed. Completion may be due to normal termination, an exception, or cancellation
-			-- in all of these cases, this method will return true.*/
-			System.out.println(stringFuture.isDone());
-			System.out.println(stringFuture.isCancelled());
+		/*
+		Returns true if this task completed. Completion may be due to normal termination, an exception, or cancellation
+		-- in all of these cases, this method will return true.*/
+		System.out.println(stringFuture.isDone());
+		System.out.println(stringFuture.isCancelled());
 
-			//stringFuture.get() 会一直阻塞，直到获取结果
-			try {
-				System.out.println(stringFuture.get(2,TimeUnit.SECONDS));
-			} catch (TimeoutException e) {
-				stringFuture.cancel(true);
-				System.out.println(stringFuture.isCancelled());
-				System.out.println(stringFuture.isDone());
-			}
+		//stringFuture.get() 会一直阻塞，直到获取结果
+		try {
+			System.out.println(stringFuture.get(2,TimeUnit.SECONDS));
+		} catch (TimeoutException e) {
+			stringFuture.cancel(true);
+			System.out.println(stringFuture.isCancelled());
+			System.out.println(stringFuture.isDone());
 		}
 	}
 }
