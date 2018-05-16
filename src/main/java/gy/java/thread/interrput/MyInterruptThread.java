9a50ee2fb21e6c62eb63f1f0235bd4c3d1083490
yantao <tao.yan@picahealth.com>
2018-05-16 15:20:32 +0800

    线程中断，中断状态

--- src/main/java/gy/java/thread/interrput/MyInterruptThread.java
+++ src/main/java/gy/java/thread/interrput/MyInterruptThread.java
@@ -17,7 +17,6 @@ public class MyInterruptThread {
 			}
 		});
 		thread.start();
-
 		System.out.println("thread.isInterrupted(1):"+thread.isInterrupted());
 		System.out.println("thread.isInterrupted(2):"+thread.isInterrupted());
 
@@ -27,11 +26,27 @@ public class MyInterruptThread {
 		System.out.println("thread.isInterrupted(3):"+thread.isInterrupted());
 		System.out.println("thread.isInterrupted(4):"+thread.isInterrupted());
 
+
 		System.out.println("\r\n");
-		thread.interrupt();
+		thread= new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println();
+				System.out.println("hello keason");
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					//InterruptedException 会清除中断状态
+					System.out.println("currentThreadInterruptStatus:"+Thread.currentThread().isInterrupted());
+					System.out.printf("hello keason catch interrupt exception");
+				}
+			}
+		});
+		thread.start();
 		System.out.println("Thread.interrupted() 会清除中断状态");
+		thread.interrupt();
 		System.out.println("thread.isInterrupted(5):"+thread.isInterrupted());
-		System.out.println("Thread.interrupted(1):"+Thread.interrupted());
+		System.out.println("thread.interrupted(1):"+ thread.interrupted());
 		System.out.println("thread.isInterrupted(6):"+thread.isInterrupted());
 
 
